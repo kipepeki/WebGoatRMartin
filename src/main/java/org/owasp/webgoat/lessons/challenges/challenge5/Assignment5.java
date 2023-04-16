64e45720396da0392649afc66163194f738c5f00
kipepeki <100298233+kipepeki@users.noreply.github.com>
2023-04-16 23:04:26 +0200

    Update Assignment5.java

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
@@ -55,19 +55,17 @@ public class Assignment5 extends AssignmentEndpoint {
             return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
         }
         try (var connection = dataSource.getConnection()) {
-            PreparedStatement statement =
-                    connection.prepareStatement(
-                            "select password from challenge_users where userid = '"
-                                    + username_login
-                                    + "' and password = '"
-                                    + password_login
-                                    + "'");
-            ResultSet resultSet = statement.executeQuery();
+            String query = "select password from challenge_users where userid = ? and password = ?";
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setString(1, username_login);
+                statement.setString(2, password_login);
+                ResultSet resultSet = statement.executeQuery(query);
 
-            if (resultSet.next()) {
-                return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
-            } else {
-                return failed(this).feedback("challenge.close").build();
+                if (resultSet.next()) {
+                    return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
+                } else {
+                    return failed(this).feedback("challenge.close").build();
+                }
             }
         }
     }
